35921cc9e3b575844b39c032784b3f923ac9739c
killerign <61916988+killerign@users.noreply.github.com>
2022-01-27 10:53:01 +0530

    comments added

--- CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/bean/Course.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/crs/flipkart/bean/Course.java
@@ -25,84 +25,84 @@ public class Course {
 	}
 
 
-	/**
+	/**Method to get count of registered students
 	 * @return the count
 	 */
 	public int getCount() {
 		return count;
 	}
 
-	/**
+	/**Method to set count of registered students
 	 * @param count the count to set
 	 */
 	public void setCount(int count) {
 		this.count = count;
 	}
 
-	/**
+	/**Method to get CourseName of a course object
 	 * @return the courseName
 	 */
 	public String getCourseName() {
 		return courseName;
 	}
 
-	/**
+	/**Method to set CourseName of a course object
 	 * @param courseName the courseName to set
 	 */
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
 	}
 
-	/**
+	/**Method to get courseId
 	 * @return the courseId
 	 */
 	public String getCourseId() {
 		return courseId;
 	}
 
-	/**
+	/**Method to set courseId
 	 * @param courseId the courseId to set
 	 */
 	public void setCourseId(String courseId) {
 		this.courseId = courseId;
 	}
 
-	/**
+	/**Method to get professorId
 	 * @return the professor
 	 */
 	public String getProfessor() {
 		return professor;
 	}
 
-	/**
+	/**Method to set ProfessorId
 	 * @param professor the professor to set
 	 */
 	public void setProfessor(String professor) {
 		this.professor = professor;
 	}
 
-	/**
+	/**Method to get isOffered status
 	 * @return the isOffered
 	 */
 	public boolean isOffered() {
 		return isOffered;
 	}
 
-	/**
+	/**Method to set isOffered status
 	 * @param isOffered the isOffered to set
 	 */
 	public void setOffered(boolean isOffered) {
 		this.isOffered = isOffered;
 	}
 
-	/**
+	/**Method to get marks 
 	 * @return the marks
 	 */
 	public double getMarks() {
 		return marks;
 	}
 
-	/**
+	/**Method to set marks
 	 * @param marks the marks to set
 	 */
 	public void setMarks(double marks) {
